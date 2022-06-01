d85a0987c304b2b7aef5a553929e63a2a2c20380
BookmDan <danoh329@gmail.com>
2022-06-01 12:18:43 -0700

    working solution

--- src/main/java/CSVReader.java
+++ src/main/java/CSVReader.java
@@ -20,13 +20,9 @@ public class CSVReader {
 //                System.out.println("index: " + values[0] + ", Year: " + values[12] + ", Score: " + values[13] + ", Rating: " + values[11] + ", Lang: " +  values[9]);
                 String key = values[0];
                 List<String> list = new ArrayList<String>();
-//                for(int i =0; i < 14; i++ ) {
-//                    list.add(values[i]);
-//                }
-                list.add(values[12]);
-                list.add(values[13]);
-                list.add(values[9]);
-                list.add(values[11]);
+                for(int i =1; i < 14; i++ ) {
+                    list.add(values[i]);
+                }
 //
 //                System.out.println(key);
 //                System.out.println(list);
@@ -51,47 +47,80 @@ public class CSVReader {
             String content_rating = reader.readLine().toUpperCase();
 
 
-            if (!language.isEmpty()) {
-                for (Map.Entry<String, List<String>> i : map.entrySet()) {
-                    if (i.getValue().contains(year) && i.getValue().contains(imdb_score)
-                            && i.getValue().contains(language) && i.getValue().contains(content_rating)) {
-                        System.out.println("Results: (Movies -> year:" + year + " score:" + imdb_score + " language:" + language + " rating:" + content_rating + ")");
-                        System.out.println(
-                                "--------------------" + "\n" +
-                                        "id: " + i.getKey() + "\n" +
-                                        "title_year: " + i.getValue().get(0) + "\n" +
-                                        "imdb_score: " + i.getValue().get(1) + "\n" +
-                                        "language: " + i.getValue().get(2) + "\n" +
-                                        "content_rating: " + i.getValue().get(3) + "\n" +
-                                        "--------------------" + "\n");
-                    } else {
-                        System.out.println(i.getKey() + i.getValue());
-                    }
-                }
-//
-            } else {
-                System.out.println("Results: (Movies -> year:" + year + " score:" + imdb_score + " rating:" + content_rating + ")");
-                while(!language.isEmpty() && !year.isEmpty() && !content_rating.isEmpty()) {
-                    System.out.println("test1");
-                    for (Map.Entry<String, List<String>> i : map.entrySet()) {
-                        System.out.println("test2");
-                        if (i.getValue().contains(year) && i.getValue().contains(imdb_score)
-                                && i.getValue().contains(content_rating)) {
-
-                            System.out.println(
-                                    "--------------------" + "\n" +
-                                            "id: " + i.getKey() + "\n" +
-                                            "title_year: " + i.getValue().get(0) + "\n" +
-                                            "imdb_score: " + i.getValue().get(1) + "\n" +
-                                            "language: " + i.getValue().get(2) + "\n" +
-                                            "content_rating: " + i.getValue().get(3) + "\n" +
-                                            "--------------------" + "\n");
-                        } else {
-                            System.out.println(i.getKey() + i.getValue());
-                        }
-                    }
+            for (Map.Entry<String, List<String>> i : map.entrySet()) {
+                if (i.getValue().contains(year) && i.getValue().contains(imdb_score)
+                        && i.getValue().contains(language)) {//&& i.getValue().contains(content_rating)) {
+                    System.out.println("Results: (Movies -> year:" + year + " score:" + imdb_score + " language:" + language + " rating:" + content_rating + ")");
+                    System.out.println(
+                            "--------------------" + "\n" +
+                                    "id: " + i.getKey() + "\n" +
+                                    "color: " + i.getValue().get(0) + "\n" +
+                                    "title: " + i.getValue().get(1) + "\n" +
+                                    "duration: " + i.getValue().get(2) + "\n" +
+                                    "director_name: " + i.getValue().get(3) + "\n" +
+                                    "act1: " + i.getValue().get(4) + "\n" +
+                                    "act2: " + i.getValue().get(5) + "\n" +
+                                    "act3: " + i.getValue().get(6) + "\n" +
+                                    "movie_imdb_link: " + i.getValue().get(7) + "\n" +
+                                    "language: " + i.getValue().get(8) + "\n" +
+                                    "country: " + i.getValue().get(9) + "\n" +
+                                    "content_rating: " + i.getValue().get(10) + "\n" +
+                                    "title_year: " + i.getValue().get(11) + "\n" +
+                                    "imdb_score: " + i.getValue().get(12) + "\n" +
+                                    "--------------------" + "\n");
                 }
             }
+//            if (!language.isEmpty()) {
+//                for (Map.Entry<String, List<String>> i : map.entrySet()) {
+//                    if (i.getValue().contains(year) && i.getValue().contains(imdb_score)
+//                            && i.getValue().contains(language) ) {//&& i.getValue().contains(content_rating)) {
+//                        System.out.println("Results: (Movies -> year:" + year + " score:" + imdb_score + " language:" + language + " rating:" + content_rating + ")");
+//                        System.out.println(
+//                                "--------------------" + "\n" +
+//                                        "id: " + i.getKey() + "\n" +
+//                                        "color: " + i.getValue().get(0) + "\n" +
+//                                        "title: " + i.getValue().get(1) + "\n" +
+//                                        "duration: " + i.getValue().get(2) + "\n" +
+//                                        "director_name: " + i.getValue().get(3) + "\n" +
+//                                        "act1: " + i.getValue().get(4) + "\n" +
+//                                        "act2: " + i.getValue().get(5) + "\n" +
+//                                        "act3: " + i.getValue().get(6) + "\n" +
+//                                        "movie_imdb_link: " + i.getValue().get(7) + "\n" +
+//                                        "language: " + i.getValue().get(8) + "\n" +
+//                                        "country: " + i.getValue().get(9) + "\n" +
+//                                        "content_rating: " + i.getValue().get(10) + "\n" +
+//                                        "title_year: " + i.getValue().get(11) + "\n" +
+//                                        "imdb_score: " + i.getValue().get(12) + "\n" +
+//                                        "--------------------" + "\n");
+//                    } else {
+//                        System.out.println(i.getKey() + i.getValue());
+//                    }
+//                }
+////
+//            } else {
+//                System.out.println("Results: (Movies -> year:" + year + " score:" + imdb_score + " rating:" + content_rating + ")");
+//                while(!language.isEmpty() && !year.isEmpty() && !content_rating.isEmpty()) {
+//                    System.out.println("test1");
+//                    for (Map.Entry<String, List<String>> i : map.entrySet()) {
+//                        System.out.println("test2");
+//                        if (i.getValue().contains(year) && i.getValue().contains(imdb_score)
+//                                && i.getValue().contains(content_rating)) {
+//
+//                            System.out.println(
+//                                    "--------------------" + "\n" +
+//                                            "id: " + i.getKey() + "\n" +
+//                                            "title_year: " + i.getValue().get(0) + "\n" +
+//                                            "imdb_score: " + i.getValue().get(1) + "\n" +
+//                                            "language: " + i.getValue().get(2) + "\n" +
+//                                            "content_rating: " + i.getValue().get(3) + "\n" +
+//                                            "--------------------" + "\n");
+//                        } else {
+//                            System.out.println(i.getKey() + i.getValue());
+//                        }
+//                    }
+//                }
+//            }
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
